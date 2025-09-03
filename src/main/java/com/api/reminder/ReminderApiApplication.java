17b60c4db993da7e1febf46fbd7695d52f54f0bc
Henrique20o <henriquepc7@gmail.com>
2025-09-03 17:41:54 -0300

    Rename package to com.api.reminder

--- src/main/java/com/api/reminder/ReminderApiApplication.java
+++ src/main/java/com/api/reminder/ReminderApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.reminder_api;
+package com.api.reminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/api/reminder/ReminderApiApplicationTests.java
+++ src/test/java/com/api/reminder/ReminderApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.reminder_api;
+package com.api.reminder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
