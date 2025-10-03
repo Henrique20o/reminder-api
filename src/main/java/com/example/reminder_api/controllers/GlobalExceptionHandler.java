5e3837db6be2b323ab26ee360a0d98e15e2151a4
Henrique20o <henriquepc7@gmail.com>
2025-10-03 16:06:03 -0300

    Add Openai

--- src/main/java/com/example/reminder_api/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/example/reminder_api/controllers/GlobalExceptionHandler.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControllerAdvice {
-
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
